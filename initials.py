5b4584a7e307bd217babc3e1451bd2d6ad8c53fb
Maxwell <maxwellakuamoah076@gmail.com>
2026-05-31 22:47:23 +0000

    Assignment completed

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
 
 def main():
-  pass #code goes here
+  pass
+#File: initials.py
+#Description: A code to print out my initials
+#Assignment No.: 2
+#
+#Name: Maxwell Akuamoah-Boateng
+#Student ID: 2425403943
+#Email: fill when acc is activated
+#Grader: Augustus Buckman
+#
+#On my honour, Maxwell Akuamoah-Boateng, this programming assignment is my own work
+#and I have not provided this code to any other student.
 
-
+print("...MAB")
+print ("")
+print ("...MMMM....MMMM............AAAA............BBBBBBBBBB.....")
+print ("...MMMM....MMMM..........AAAAAAAA..........BBB.....BBB....")
+print ("...MM.MM..MM.MM........AAAA....AAAA........BBB.....BBB....")
+print ("...MM.MM..MM.MM........AAAA....AAAA........BBB.....BBB....")
+print ("...MM.MM..MM.MM........AAAA....AAAA........BBB.....BBB....")
+print ("...MM..MMMM..MM........AAAAAAAAAAAA........BBBBBBBBB......")
+print ("...MM..MMMM..MM........AAAA....AAAA........BBB.....BBB....")
+print ("...MM...MM...MM........AAAA....AAAA........BBB.....BBB....")
+print ("...MM...MM...MM...**...AAAA....AAAA...**...BBB.....BBB....")
+print ("...MM........MM...**...AAAA....AAAA...**...BBBBBBBBBB.....")
+print("")
 
 main()
